6a56e4786f954b3bd7fe9a3eb2d5b2f2dcd9f176
MarcosDavi-Dev <adamsdavi77@gmail.com>
2025-01-21 21:27:44 -0300

    Complete

--- src/components/MyForm.jsx
+++ src/components/MyForm.jsx
@@ -7,6 +7,10 @@ const MyForm = ({ userName, userEmail }) => {
   const [name, setName] = useState(userName);
   const [email, setEmail] = useState(userEmail);
 
+  const [bio, setBio] = useState("");
+
+  const [role, setRole] = useState("");
+
   const handleName = (e) => {
     setName(e.target.value);
   };
@@ -15,11 +19,13 @@ const MyForm = ({ userName, userEmail }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    console.log(name, email);
+    console.log(name, email, bio, role);
 
     // 7 - Limpar o form
     setName("");
     setEmail("");
+    setBio("");
+    setRole("");
   };
 
   return (
@@ -50,6 +56,29 @@ const MyForm = ({ userName, userEmail }) => {
             // 6 - Controlled input
             value={email}
           />
+        </label>
+        {/* 8 - textarea */}
+        <label>
+          <span>Bio:</span>
+          <textarea
+            name="bio"
+            placeholder="Descrição do usuário"
+            onChange={(e) => setBio(e.target.value)}
+            value={bio}
+          ></textarea>
+        </label>
+        {/* 9 - select */}
+        <label>
+          <span>Função no sistema</span>
+          <select
+            name="role"
+            onChange={(e) => setRole(e.target.value)}
+            value={role}
+          >
+            <option value="user">Usuário</option>
+            <option value="editor">Editor</option>
+            <option value="admin">Administrador</option>
+          </select>
         </label>
         <input type="submit" value="Enviar" />
       </form>
